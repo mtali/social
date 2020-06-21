8c3e319117c84ba396946c8e8a8bbc140b687284
mtali <emmanuelmtalig@gmail.com>
2020-06-22 00:22:11 +0300

    Implement follow system

--- account/urls.py
+++ account/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('register', views.register, name='register'),
     path('edit/', views.edit, name='edit'),
     path('users/', views.user_list, name='user_list'),
+    path('users/follow/', views.user_follow, name='user_follow'),
     path('users/<username>/', views.user_detail, name='user_detail'),
     path('', include('django.contrib.auth.urls')),
 
--- account/views.py
+++ account/views.py
@@ -1,9 +1,12 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_POST
 
+from common.decorators import ajax_required
 from .forms import UserRegistrationForm, UserEditForm, ProfileEditForm
-from .models import Profile, User
+from .models import Profile, User, Contact
 
 
 @login_required
@@ -75,3 +78,23 @@ def user_detail(request, username):
     return render(request,
                   'account/user/detail.html',
                   {'section': 'people', 'user': user})
+
+
+@ajax_required
+@require_POST
+@login_required
+def user_follow(request):
+    # TODO: Test user with username 'user'
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(user_from=request.user, user_to=user)
+            else:
+                Contact.objects.filter(user_from=request.user, user_to=user).delete()
+            return JsonResponse({'status': 'ok'})
+        except User.DoesNotExist:
+            return JsonResponse({'status': 'error'})
+    return JsonResponse({'status': 'error'})
