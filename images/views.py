dc85240121d06c1f422ebe7c89c580135530fc40
mtali <emmanuelmtalig@gmail.com>
2020-06-28 15:53:58 +0300

    Implement image ranking

--- images/views.py
+++ images/views.py
@@ -52,6 +52,8 @@ def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     # Increment total image views by 1
     total_views = r.incr(f'image:{image.pk}:views')
+    # Increment image ranking by 1
+    r.zincrby('image_ranking', 1, image.pk)
     return render(request,
                   'images/image/detail.html',
                   {
@@ -107,3 +109,19 @@ def image_list(request):
     return render(request,
                   f'images/image/{template}',
                   {'section': 'images', 'images': images})
+
+
+@login_required
+def image_ranking(request):
+    # Get image ranking dictionary
+    ranking = r.zrange('image_ranking', 0, -1, desc=True)[:10]
+    ranking_ids = [int(id) for id in ranking]
+    # Get most viewed images
+    most_viewed = list(Image.objects.filter(id__in=ranking_ids))
+    most_viewed.sort(key=lambda x: ranking_ids.index(x.id))
+    return render(request,
+                  'images/image/ranking.html',
+                  {
+                      'section': 'images',
+                      'most_viewed': most_viewed
+                  })
